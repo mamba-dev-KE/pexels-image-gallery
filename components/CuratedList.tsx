bd8a7935252d634969ffa38bab9f5cc2ffb4f090
Joseph Maramba <joseph@smartmile.eu>
2022-09-24 14:03:07 +0300

    code refactor and add logo

--- components/CuratedList.tsx
+++ components/CuratedList.tsx
@@ -1,5 +1,5 @@
-import { Grid, Paper, Text } from '@mantine/core';
 import Image from 'next/image';
+import { Grid, Paper, Text } from '@mantine/core';
 import { useCuratedPhotos } from 'hooks';
 
 export const CuratedList = () => {
@@ -13,17 +13,29 @@ export const CuratedList = () => {
             pb="lg"
             sx={{
               backgroundColor: 'hsl(0 0% 100% / 0.2)',
+              borderRadius: '10px',
+              overflow: 'hidden',
             }}
           >
             <Image
               src={src.portrait}
               width={400}
-              height={600}
+              height={650}
               alt=""
               objectFit="cover"
               loading="lazy"
             />
-            <Text align="center" mt="sm" size="md" weight={600}>
+            <Text
+              align="center"
+              mt="sm"
+              size="md"
+              weight={600}
+              sx={{
+                fontFamily: 'Euphoria Script',
+                fontSize: '1.5rem',
+                letterSpacing: '5px',
+              }}
+            >
               By {photographer}
             </Text>
           </Paper>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import type { AppProps } from 'next/app';
 import {
   MantineProvider,
   ColorSchemeProvider,
@@ -6,8 +8,8 @@ import {
 import { useLocalStorage } from '@mantine/hooks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import type { AppProps } from 'next/app';
-import { useState } from 'react';
+import '@fontsource/euphoria-script';
+import '@fontsource/aleo';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient());
